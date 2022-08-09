86fcfdc00b1786e9a2984134e17cd4af7e81f729
Igor Obrovac <igor.obrovac990@gmail.com>
2022-08-09 10:49:17 +0200

    Treci pokusaj

--- pages/random.tsx
+++ pages/random.tsx
@@ -1,18 +1,11 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import styles from "../styles/Random.module.scss";
-import HeaderContact from "../components/HeaderContact";
-import HeaderNav from "../components/HeaderNav";
-import Footer from "../components/Footer";
-import React, { useState } from "react";
 
 const Random: NextPage = () => {
   return (
-    <div className={styles.homepageContainer}>
+    <>
       <h1>OVO JE SAMO DA SE VIDI TEST</h1>
-      <HeaderContact />
-      <HeaderNav />
-    </div>
+    </>
   );
 };
 
